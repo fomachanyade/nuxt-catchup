280af355d109bd8dff01672aa6b95601b238c0a2
katakanakun <fomachanyade@gmail.com>
2021-02-24 16:32:35 +0900

    MOD: show video without player

--- pages/vimeo.vue
+++ pages/vimeo.vue
@@ -1,24 +1,41 @@
 <template>
-  <div>
-    <h1>Vimeo API</h1>
+  <client-only>
     <div>
-      <h2>get content from vimeo</h2>
-      <p>url: https://vimeo.com/65720665</p>
-      <template v-if="videoHtml">
-        <div v-html="videoHtml"></div>
-      </template>
+      <h1>Vimeo API</h1>
+      <div>
+        <h2>get content from vimeo</h2>
+        <p>url: https://vimeo.com/65720665</p>
+        <template v-if="videoHtml">
+          <div v-html="videoHtml"></div>
+        </template>
+        <button v-on:click="showPlayer">showPlayer</button>
+        <div id="myVideo" style="width: 800px; height: 800px"></div>
+      </div>
+      <div>
+        <h2>upload</h2>
+        <button v-on:click="uploadVideo">upload video</button>
+        <pre>{{ uploadResult }}</pre>
+      </div>
+      <NuxtLink to="/"> Home page </NuxtLink>
     </div>
-    <NuxtLink to="/"> Home page </NuxtLink>
-  </div>
+  </client-only>
 </template>
+<script src="https://player.vimeo.com/api/player.js"></script>
 <script>
+import Player from '@vimeo/player'
+let Vimeos = require('vimeo').Vimeo
+
+let client = new Vimeos(
+  '961b222cac5d10f12d83e003b342df78bd35ef60',
+  'KeF0FjKgui7eO9rIxgKnb7EnUOavO9FGrV9dP1uFUxHL12N/ywI0Itqm1AawklZ6mEGRp+vhc8ieW0cdY3nsTczglhgApVl5ErwNIi96j77Ig4t09d9Cw3OlKYf04L7L',
+  '648a6ef5e49d167d2bf730be50ec1943'
+)
 export default {
   data: function () {
-    return { videoHtml: '' }
+    return { videoHtml: '', uploadResult: '', videoId: '' }
   },
   asyncData() {
-    let Vimeo = require('vimeo').Vimeo
-    let client = new Vimeo(
+    client = new Vimeos(
       '961b222cac5d10f12d83e003b342df78bd35ef60',
       'KeF0FjKgui7eO9rIxgKnb7EnUOavO9FGrV9dP1uFUxHL12N/ywI0Itqm1AawklZ6mEGRp+vhc8ieW0cdY3nsTczglhgApVl5ErwNIi96j77Ig4t09d9Cw3OlKYf04L7L',
       '648a6ef5e49d167d2bf730be50ec1943'
@@ -44,13 +61,54 @@ export default {
   },
   async fetch() {
     const contentUrl = 'https://vimeo.com/65720665'
-    const requestUrl = `https://vimeo.com/api/oembed.json?url=${contentUrl}`
+    const requestUrl = `https://vimeo.com/api/oembed.json?url=${contentUrl}&background=true`
     const response = await this.$axios.$get(requestUrl)
     console.log(response)
+
     this.videoHtml = response.html
+    this.videoId = response.video_id
   },
   methods: {
-    uploadVideo: function (path) {},
+    uploadVideo: function (path) {
+      const self = this
+      const uploadOption = {
+        name: 'test',
+        privacy: {
+          embed: 'whitelist',
+          view: 'unlisted',
+        },
+      }
+      path = require('../assets/video/test.mp4')
+      client.upload(
+        path,
+        uploadOption,
+        function (uri) {
+          console.log('File upload completed. Your Vimeo URI is:', uri)
+          self.uploadResult = uri
+        },
+        function (bytesUploaded, bytesTotal) {
+          var percentage = ((bytesUploaded / bytesTotal) * 100).toFixed(2)
+          console.log(bytesUploaded, bytesTotal, percentage + '%')
+          self.uploadResult = `${percentage}% done...`
+        },
+        function (error) {
+          console.log('Failed because: ' + error)
+          self.uploadResult = error
+        }
+      )
+    },
+    showPlayer: function () {
+      const options = {
+        id: this.videoId,
+        width: 800,
+        autoplay: true,
+        background: true,
+      }
+      const videoPlayer = new Player('myVideo', options)
+      videoPlayer.on('play', function () {
+        console.log('Played the video')
+      })
+    },
   },
 }
 </script>
